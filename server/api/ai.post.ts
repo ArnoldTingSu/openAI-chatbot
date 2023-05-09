c73586063730ebb7c8e76b6c228953e8949fc70e
Arnold Su <arnoldtingsu@gmail.com>
2023-05-08 23:45:25 -0700

    URL generative commentary

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,7 +1,13 @@
 import { Configuration, OpenAIApi } from "openai";
-export default defineEventHandler(async (event) => {
+import * as agents from "@/agents"
 
+export default defineEventHandler(async (event) => {
   const body = await readBody(event);
+  const agent = body.agent || "customerSupportAgent"
+
+  if(!Object.keys(agents).includes(agent)){
+    throw new Error(`${agent} doesn't exist`);
+  }
 
 const {OPENAI_API_KEY} = useRuntimeConfig();
 
@@ -14,6 +20,8 @@ const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
   messages: body.messages || [],
   temperature: body.temperature || 1,
+  // @ts-expect-error checking above if the agent exists
+  ...agents[agent](body),
 });
   return completion.data;
 });
